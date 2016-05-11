de7725af683ab3d4ba8d312bc53c0f1222622940
John Marinelli <john@Macs-MacBook-Pro-3.local>
2016-05-10 20:21:28 -0700

    this will be pretty worthless

--- src/cljs/stats/core.cljs
+++ src/cljs/stats/core.cljs
@@ -1,5 +1,6 @@
 (ns stats.core
-  (:require [domina.core :refer [by-id value set-value!]]))
+  (:require [domina.core :refer [by-id value set-value!]]
+            [clojure.string :as str]))
 
 (enable-console-print!)
 
@@ -12,14 +13,33 @@
         width (- (count within) 1)]
     (nth within (mod (rand-int 100) width))))
 
+(defn get-mean [coll]
+  (/ (reduce + coll) (count coll)))
+
 (defn random-mean [begin end n]
   (let [rand-series (map #(random rand-min rand-max) (range 0 n))]
-    (/ (reduce + rand-series) (count rand-series))))
+    (get-mean rand-series)))
 
+(defn population-std-deviation 
+  ([coll sample]
+   (let [n (if sample (- (count coll) 1) (count coll))
+         mean (get-mean coll)
+         sum (reduce + (map #(Math/pow (- % mean) 2) coll))]
+     (Math/pow (/ sum n) 0.5)))
+  ([coll] (population-std-deviation coll false)))
+  
 (defn set-random-mean-html! 
   []
   (let [rm (random-mean rand-min rand-max sample-size)]
     (set-value! (by-id "random-mean") rm)))
+
+(defn set-population-standard-deviation-html!
+  []
+  "Reads a csv list of values and writes the S.D of them to the screen"
+  (let [psd-input (value (by-id "pop-sd-sample-input"))
+        sample-coll (map #(js/parseInt %) (str/split psd-input #","))
+        psd-output (by-id "pop-sd-sample-result")]
+    (population-std-deviation sample-coll true)))
 
 (defn init []
   (let [random-mean-generator (by-id "generate-random-mean")]
